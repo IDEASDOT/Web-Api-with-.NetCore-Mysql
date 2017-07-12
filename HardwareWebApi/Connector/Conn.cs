20d33942d4693b351207348f03499ba180bc3e86
VB <vb@VB10.local>
2017-07-12 21:59:34 +0300

    Web api v1
    Web api backend code

---
+++ HardwareWebApi/Connector/Conn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using HardwareWebApi.Model;
+using Pomelo.Data.MySql;
+
+namespace HardwareWebApi.Connector
+{
+    public class Conn
+    {
+        private string connstring;
+        public Conn()
+        {
+			//connstring = @"server=sql12.freemysqlhosting.net;userid=;password=;database=sql12174637";
+			connstring = @"server=localhost;userid=root;password=;database=vbislife";
+        }
+
+        public List<User> UserList(){
+
+            List<User> allUser = new List<User>();
+
+
+            using (MySqlConnection connMysql = new MySqlConnection(connstring)){
+
+                using (MySqlCommand cmdd = connMysql.CreateCommand()){
+
+
+                    cmdd.CommandText = "Select * from userlist";
+                    cmdd.CommandType = System.Data.CommandType.Text;
+
+                    cmdd.Connection = connMysql;
+
+                    connMysql.Open();
+
+                    using(MySqlDataReader reader = cmdd.ExecuteReader() ){
+
+                        while (reader.Read())
+                        {
+                            allUser.Add(new User{                       id=reader.GetInt32(reader.GetOrdinal("id"))
+                                                                        ,name=reader.GetString(reader.GetOrdinal("name"))
+                                                                       ,password=reader.GetString(reader.GetOrdinal("password"))});
+                        }
+                    }
+                }
+
+                connMysql.Close();
+            }
+
+
+
+            return allUser;
+
+        }
+
+
+    }
+}
---
+++ HardwareWebApi/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HardwareWebApi.Model
+{
+    public class User
+    {
+       
+        public int id
+        {
+            get;
+            set;
+        }
+        public string name
+        {
+            get;
+            set;
+        }
+
+        public string password
+        {
+            get;
+            set;
+        }
+    }
+}
+
